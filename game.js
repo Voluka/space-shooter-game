d950b91fd2eb86c2eacf6cd2d4c452e490cf8894
Voluka <72554370+Voluka@users.noreply.github.com>
2025-08-20 17:41:45 +0300

    Update game.js

--- game.js
+++ game.js
@@ -5,6 +5,16 @@ const ctx = canvas.getContext("2d");
 canvas.width = 320;
 canvas.height = 480;
 
+// Загрузка изображений
+const playerImg = new Image();
+playerImg.src = 'assets/player.png';
+
+const enemyImg = new Image();
+enemyImg.src = 'assets/enemy.png';
+
+const bulletImg = new Image();
+bulletImg.src = 'assets/bullet.png';
+
 // Игрок
 const player = {
   x: canvas.width / 2 - 15,
@@ -34,8 +44,7 @@ window.addEventListener("keyup", (e) => {
 
 // Отрисовка игрока
 function drawPlayer() {
-  ctx.fillStyle = player.color;
-  ctx.fillRect(player.x, player.y, player.width, player.height);
+  ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
 }
 
 // Движение игрока
@@ -67,8 +76,7 @@ function shoot() {
 function drawBullets() {
   bullets.forEach((bullet, index) => {
     bullet.y -= bullet.speed;
-    ctx.fillStyle = bullet.color;
-    ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+    ctx.drawImage(bulletImg, bullet.x, bullet.y, bullet.width, bullet.height);
 
     // Удаляем пулю, если вышла за экран
     if (bullet.y < 0) {
@@ -95,14 +103,56 @@ function spawnEnemy() {
 function drawEnemies() {
   enemies.forEach((enemy, index) => {
     enemy.y += enemy.speed;
-    ctx.fillStyle = enemy.color;
-    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+    ctx.drawImage(enemyImg, enemy.x, enemy.y, enemy.width, enemy.height);
 
     // Удаляем врага, если вышел за экран
     if (enemy.y > canvas.height) {
       enemies.splice(index, 1);
     }
   });
+}
+
+// Проверка столкновений
+function checkCollisions() {
+  // Пуля <-> Враг
+  for (let i = bullets.length - 1; i >= 0; i--) {
+    for (let j = enemies.length - 1; j >= 0; j--) {
+      if (
+        bullets[i].x < enemies[j].x + enemies[j].width &&
+        bullets[i].x + bullets[i].width > enemies[j].x &&
+        bullets[i].y < enemies[j].y + enemies[j].height &&
+        bullets[i].y + bullets[i].height > enemies[j].y
+      ) {
+        // Столкновение!
+        bullets.splice(i, 1);
+        enemies.splice(j, 1);
+        score += 10;
+        break;
+      }
+    }
+  }
+
+  // Игрок <-> Враг
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    if (
+      player.x < enemies[i].x + enemies[i].width &&
+      player.x + player.width > enemies[i].x &&
+      player.y < enemies[i].y + enemies[i].height &&
+      player.y + player.height > enemies[i].y
+    ) {
+      // Игрок уничтожен
+      alert("Game Over! Счёт: " + score);
+      document.location.reload();
+      return;
+    }
+  }
+}
+
+// Отображение счёта
+function drawScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "16px Arial";
+  ctx.fillText("Счёт: " + score, 10, 20);
 }
 
 // Главный игровой цикл
@@ -114,8 +164,13 @@ function gameLoop() {
   drawBullets();
   spawnEnemy();
   drawEnemies();
+  checkCollisions();
+  drawScore();
 
   requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+// Начинаем игру, когда изображения загружены
+playerImg.onload = () => {
+  gameLoop();
+};
